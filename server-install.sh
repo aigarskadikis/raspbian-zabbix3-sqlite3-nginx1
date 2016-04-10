4d8409d8dbddc4528c8c982c4277a28bb9a74029
catonrug <aigars.kadikis@gmail.com>
2016-04-10 17:13:09 +0300

    next

--- server-install.sh
+++ server-install.sh
@@ -9,6 +9,59 @@
 #update repositories and upgrade system
 apt-get update -y && apt-get upgrade -y
 
+#install additional libraries for nginx
+apt-get install php5-fpm libgd2-xpm-dev libpcrecpp0 libxpm4 -y
+
+#install nginx web server
+apt-get install nginx -y
+
+#Install the CGI wrapper if you need to run CGI scripts on your server
+apt-get install fcgiwrap -y
+
+#Reduce the number of PHP and CGI processes to free up memory
+sed -i "s/^.*process\.max = .*$/process.max = 2/" /etc/php5/fpm/php-fpm.conf
+
+#Modify the maximum number of processes. In my case I won't have dozens of connections therefore file 
+sed -i "s/^worker_processes .*;$/worker_processes 1;/" /etc/nginx/nginx.conf
+
+#create directory for web server
+mkdir -p /var/www/html
+
+#set web server user to be owner of frontent
+chown -R www-data:www-data /var/www/html
+
+#create php sample
+echo "<?php phpinfo(); ?>" > /var/www/html/index.php
+
+#backup original configuration
+cp /etc/nginx/sites-available/{default,original}
+
+#replace active configuration
+cat > /etc/nginx/sites-available/default << EOF
+server {
+
+listen 80 default_server;
+listen [::]:80 default_server;
+
+root /var/www/html;
+
+index index.php index.html index.htm;
+
+server_name _;
+
+location ~ \.php\$ {
+include snippets/fastcgi-php.conf;
+fastcgi_pass unix:/var/run/php5-fpm.sock;
+}
+
+}
+EOF
+
+#restart nginx service
+service nginx restart
+service php5-fpm restart
+service fcgiwrap restart
+
 #install zabbix server prerequisites
 apt-get install php5 php5-dev php5-gd -y
 apt-get install fping -y
@@ -62,54 +115,6 @@ sed -i "s/^.*AlertScriptsPath=.*$/AlertScriptsPath=\/var\/zabbix\/alertscripts/"
 sed -i "s/^.*ExternalScripts=.*$/ExternalScripts=\/var\/zabbix\/externalscripts/" /usr/local/etc/zabbix_server.conf
 sed -i "s/^LogFile=.*$/LogFile=\/var\/log\/zabbix\/zabbix_server.log/" /usr/local/etc/zabbix_server.conf
 sed -i "s/^DBName=.*$/DBName=\/var\/lib\/sqlite\/zabbix.db/" /usr/local/etc/zabbix_server.conf
-
-#install additional libraries for nginx
-apt-get install php5-fpm libgd2-xpm-dev libpcrecpp0 libxpm4 -y
-
-#install nginx web server
-apt-get install nginx -y
-
-#Install the CGI wrapper if you need to run CGI scripts on your server
-apt-get install fcgiwrap -y
-
-#Reduce the number of PHP and CGI processes to free up memory
-sed -i "s/^.*process\.max = .*$/process.max = 2/" /etc/php5/fpm/php-fpm.conf
-
-#Modify the maximum number of processes. In my case I won't have dozens of connections therefore file 
-sed -i "s/^worker_processes .*;$/worker_processes 1;/" /etc/nginx/nginx.conf
-
-#create directory for web server
-mkdir -p /var/www/html
-
-#set web server user to be owner of frontent
-chown -R www-data:www-data /var/www/html
-
-#create php sample
-echo "<?php phpinfo(); ?>" > /var/www/html/index.php
-
-#backup original configuration
-cp /etc/nginx/sites-available/{default,original}
-
-#replace active configuration
-cat > /etc/nginx/sites-available/default << EOF
-server {
-
-listen 80 default_server;
-listen [::]:80 default_server;
-
-root /var/www/html;
-
-index index.php index.html index.htm;
-
-server_name _;
-
-location ~ \.php\$ {
-include snippets/fastcgi-php.conf;
-fastcgi_pass unix:/var/run/php5-fpm.sock;
-}
-
-}
-EOF
 
 mkdir -p /var/www/html/zabbix
 cd ~/zabbix-*/frontends/php/
